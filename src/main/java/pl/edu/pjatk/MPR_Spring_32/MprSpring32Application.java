161b8f22cfedc24a175500256cd955b2ac83463b
s30274 <s30274@pjwstk.edu.pl>
2024-10-07 11:10:50 +0200

    First commit

---
+++ src/main/java/pl/edu/pjatk/MPR_Spring_32/MprSpring32Application.java
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.MPR_Spring_32;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MprSpring32Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MprSpring32Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/pjatk/MPR_Spring_32/MprSpring32ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.MPR_Spring_32;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MprSpring32ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
